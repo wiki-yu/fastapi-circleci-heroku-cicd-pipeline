17952a8bde61af73bd0af6bc9ad5afcaf2aac1fb
wiki-yu <xyu0127@gmail.com>
2022-03-16 16:56:29 -0500

    update main

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message`": "Hello World111111111!"}
+    return {"message`": "Hello World2222222!"}
 
 
 app.include_router(api_router, prefix="/api/v1")
